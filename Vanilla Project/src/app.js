a95ed778e013e6387f3210e54187a50ad6c87fb0
krisztaramn <60409575+krisztaramn@users.noreply.github.com>
2020-04-04 18:10:02 +0100

    iconElement added

--- Vanilla Project/src/app.js
+++ Vanilla Project/src/app.js
@@ -30,14 +30,20 @@ function displayWeather(response) {
   let description = document.querySelector("#description");
   let timeElement = document.querySelector("#time");
   let currentDay = document.querySelector(".current-day");
+  let iconElement = document.querySelector("#icon-today");
 
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   h1.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   timeElement.innerHTML = formatDate(time);
   currentDay.innerHTML = showDay();
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "New York";
+let city = "Los angeles";
 let apiKey = "c30b70227281106a3ea7b3125756ea7f";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(url).then(displayWeather);
