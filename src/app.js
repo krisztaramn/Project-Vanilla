6848a7dd3be333e232a3a50afa8c029cd5afc47a
krisztaramn <60409575+krisztaramn@users.noreply.github.com>
2020-04-05 16:48:21 +0100

    forcast icons added

--- src/app.js
+++ src/app.js
@@ -50,6 +50,9 @@ function search(city) {
   let apiKey = "c30b70227281106a3ea7b3125756ea7f";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(displayWeather);
+
+  let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(forecastUrl).then(displayForecast);
 }
 
 function showCity(event) {
@@ -74,6 +77,63 @@ function showCelsius(event) {
   document.getElementById("fahrenheit").style.fontSize = "1em";
 }
 
+function showCurrentLocation(event) {
+  event.preventDefault();
+  function showPosition(position) {
+    let apiKey = "c30b70227281106a3ea7b3125756ea7f";
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    axios.get(url).then(displayWeather);
+  }
+
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function displayForecast(response) {
+  let secondDay = document.querySelector("#second-temperature");
+  let thirdDay = document.querySelector("#third-temperature");
+  let fourthDay = document.querySelector("#fourth-temperature");
+  let fifthDay = document.querySelector("#fifth-temperature");
+  let sixthDay = document.querySelector("#sixth-temperature");
+  let secondIcon = document.querySelector("#second-icon");
+  let thirdIcon = document.querySelector("#third-icon");
+  let fourthIcon = document.querySelector("#fourth-icon");
+  let fifthIcon = document.querySelector("#fifth-icon");
+  let sixthIcon = document.querySelector("#sixth-icon");
+
+  secondDay.innerHTML = Math.round(response.data.list[7].main.temp);
+  thirdDay.innerHTML = Math.round(response.data.list[15].main.temp);
+  fourthDay.innerHTML = Math.round(response.data.list[23].main.temp);
+  fifthDay.innerHTML = Math.round(response.data.list[31].main.temp);
+  sixthDay.innerHTML = Math.round(response.data.list[39].main.temp);
+  secondIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.list[7].weather[0].icon}@2x.png`
+  );
+  secondIcon.setAttribute("alt", response.data.list[7].weather[0].description);
+  thirdIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.list[15].weather[0].icon}@2x.png`
+  );
+  thirdIcon.setAttribute("alt", response.data.list[15].weather[0].description);
+  fourthIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.list[23].weather[0].icon}@2x.png`
+  );
+  fourthIcon.setAttribute("alt", response.data.list[23].weather[0].description);
+  fifthIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.list[31].weather[0].icon}@2x.png`
+  );
+  fifthIcon.setAttribute("alt", response.data.list[31].weather[0].description);
+  sixthIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.list[39].weather[0].icon}@2x.png`
+  );
+  sixthIcon.setAttribute("alt", response.data.list[39].weather[0].description);
+}
+
 let celsiusValue = null;
 
 let form = document.querySelector("form");
@@ -84,3 +144,8 @@ fahrenheit.addEventListener("click", showFahrenheit);
 
 let celsius = document.querySelector(".celsius");
 celsius.addEventListener("click", showCelsius);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", showCurrentLocation);
+
+search("Lisbon");
